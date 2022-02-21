84b2af317b833ffa9f1ea373016d0f2dbaa81510
alexandre renaud <alexandre.renaud@yahoo.fr>
2022-02-21 17:57:56 +0100

    lint : fix basic lint error

--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -46,6 +46,7 @@ func remove(lis []packet, pack *packet) []packet {
 	return dest
 }
 
+// colorWeight get the color to print with the weight
 func colorWeight(weight uint16) string {
 	if weight == 500 {
 		return "BLUE"
@@ -55,7 +56,7 @@ func colorWeight(weight uint16) string {
 	return "YELLOW"
 }
 
-func (a algorithm) createMap() (err error) {
+func (a algorithm) createMap() {
 	for i := 0; i < int(a.Ware.X); i++ {
 		for j := 0; j < int(a.Ware.Y); j++ {
 			a.TwoDMap[i][j] = 0
@@ -70,7 +71,6 @@ func (a algorithm) createMap() (err error) {
 	for _, trk := range a.Listtruck {
 		a.TwoDMap[trk.X][trk.Y] = 3
 	}
-	return nil
 }
 
 func (a algorithm) findPacket(x uint16, y uint16) *packet {
@@ -82,7 +82,7 @@ func (a algorithm) findPacket(x uint16, y uint16) *packet {
 	return nil
 }
 
-func (a algorithm) gotopacket(palIndex int, packIndex int) (err error) {
+func (a algorithm) gotopacket(palIndex int, packIndex int) {
 	var ptr *packet
 	if a.TwoDMap[a.Listpal[palIndex].Y][a.Listpal[palIndex].X+1] == 2 {
 		ptr = a.findPacket(a.Listpal[palIndex].Y, a.Listpal[palIndex].X+1)
@@ -95,29 +95,29 @@ func (a algorithm) gotopacket(palIndex int, packIndex int) (err error) {
 	}
 	if ptr == nil {
 		if (a.Listpal[palIndex].Y < a.Listtruck[packIndex].Y) && (a.TwoDMap[a.Listpal[palIndex].Y-1][a.Listpal[palIndex].X] == 0) {
-			a.Listpal[palIndex].Y -= 1
+			a.Listpal[palIndex].Y--
 		} else if (a.Listpal[palIndex].X > a.Listtruck[packIndex].X) && (a.TwoDMap[a.Listpal[palIndex].Y][a.Listpal[palIndex].X-1] == 0) {
-			a.Listpal[palIndex].X -= 1
+			a.Listpal[palIndex].X--
 		} else if (a.Listpal[palIndex].Y < a.Listtruck[packIndex].Y) && (a.TwoDMap[a.Listpal[palIndex].Y+1][a.Listpal[palIndex].X] == 0) {
-			a.Listpal[palIndex].Y += 1
+			a.Listpal[palIndex].Y++
 		} else if (a.Listpal[palIndex].X < a.Listtruck[packIndex].X) && (a.TwoDMap[a.Listpal[palIndex].Y][a.Listpal[palIndex].X+1] == 0) {
-			a.Listpal[palIndex].X += 1
+			a.Listpal[palIndex].X++
 		} else {
 			a.Listpal[palIndex].Command = fmt.Sprintf("%s WAIT\n", a.Listpal[palIndex].Name)
-			return nil
+			return
 		}
 		a.Listpal[palIndex].Command = fmt.Sprintf("%s GO [%d,%d]\n", a.Listpal[palIndex].Name, a.Listpal[palIndex].X, a.Listpal[palIndex].Y)
 	} else {
 		a.TwoDMap[ptr.X][ptr.Y] = 0
-		a.Listpacket = remove(a.Listpacket, ptr)
+		remove(a.Listpacket, ptr)
 		a.Listpal[palIndex].Carry = true
 		a.Listpal[palIndex].Pack = ptr
-		fmt.Print("%s TAKE %s %s\n")
+		fmt.Printf("%s TAKE %s %s\n", a.Listpal[palIndex].Name, ptr.Name, colorWeight(ptr.Weight))
 	}
-	return nil
 }
 
-func (a algorithm) gototruck(palIndex int, truckIndex int) (err error) {
+// gototruck make the palette move to the truck
+func (a algorithm) gototruck(palIndex int, truckIndex int) {
 	if Abs(int(a.Listpal[palIndex].X)-int(a.Listtruck[truckIndex].X))+(Abs(int(a.Listpal[palIndex].Y)-int(a.Listtruck[truckIndex].Y))) == 1 {
 		if uint32(a.Listpal[palIndex].Pack.Weight) > a.Listtruck[truckIndex].MaxContent-a.Listtruck[truckIndex].Content {
 			a.Listpal[palIndex].Command = fmt.Sprintf("%s LEAVE %s %s\n", a.Listpal[palIndex].Name, a.Listpal[palIndex].Pack.Name, colorWeight(a.Listpal[palIndex].Pack.Weight))
@@ -128,23 +128,23 @@ func (a algorithm) gototruck(palIndex int, truckIndex int) (err error) {
 		}
 	} else {
 		if (a.Listpal[palIndex].X < a.Listtruck[truckIndex].X) && (a.TwoDMap[a.Listpal[palIndex].Y][a.Listpal[palIndex].X+1] == 0) {
-			a.Listpal[palIndex].X += 1
+			a.Listpal[palIndex].X++
 		} else if (a.Listpal[palIndex].Y < a.Listtruck[truckIndex].Y) && (a.TwoDMap[a.Listpal[palIndex].Y+1][a.Listpal[palIndex].X] == 0) {
-			a.Listpal[palIndex].Y += 1
+			a.Listpal[palIndex].Y++
 		} else if (a.Listpal[palIndex].X > a.Listtruck[truckIndex].X) && (a.TwoDMap[a.Listpal[palIndex].Y][a.Listpal[palIndex].X-1] == 0) {
-			a.Listpal[palIndex].X -= 1
+			a.Listpal[palIndex].X--
 		} else if (a.Listpal[palIndex].Y < a.Listtruck[truckIndex].Y) && (a.TwoDMap[a.Listpal[palIndex].Y-1][a.Listpal[palIndex].X] == 0) {
-			a.Listpal[palIndex].Y -= 1
+			a.Listpal[palIndex].Y--
 		} else {
 			a.Listpal[palIndex].Command = fmt.Sprintf("%s WAIT\n", a.Listpal[palIndex].Name)
-			return nil
+			return
 		}
 		a.TwoDMap[a.Listpal[palIndex].X][a.Listpal[palIndex].Y] = 1
 		a.Listpal[palIndex].Command = fmt.Sprintf("%s GO [%d,%d]\n", a.Listpal[palIndex].Name, a.Listpal[palIndex].X, a.Listpal[palIndex].Y)
 	}
-	return nil
 }
 
+// isEmpty know if there is no more packet left
 func (a algorithm) isEmpty() bool {
 	for _, i := range a.TwoDMap {
 		for _, j := range i {
@@ -161,6 +161,7 @@ func (a algorithm) isEmpty() bool {
 	return true
 }
 
+// Abs get the absolute value of a int
 func Abs(value int) int {
 	if value < 0 {
 		return -value
@@ -168,7 +169,8 @@ func Abs(value int) int {
 	return value
 }
 
-func get_truck(distance int, pal palette, truckList []truck) int {
+// getTruck get the closest truck of a palette
+func getTruck(distance int, pal palette, truckList []truck) int {
 	ret := -1
 	for i := 0; i < len(truckList); i++ {
 		tmp := (Abs(int(pal.X) - int(truckList[i].X))) + (Abs(int(pal.Y) - int(truckList[i].Y)))
@@ -180,13 +182,14 @@ func get_truck(distance int, pal palette, truckList []truck) int {
 	return ret
 }
 
-func get_pal(distance int, pack packet, Listpal []palette) int {
+// getPal get the closest palette of a packet
+func getPal(distance int, pack packet, listpal []palette) int {
 	ret := -1
-	for i := 0; i < len(Listpal); i++ {
-		if Listpal[i].Carry {
+	for i := 0; i < len(listpal); i++ {
+		if listpal[i].Carry {
 			continue
 		}
-		tmp := (Abs(int(pack.X) - int(Listpal[i].X))) + (Abs(int(pack.Y) - int(Listpal[i].Y)))
+		tmp := (Abs(int(pack.X) - int(listpal[i].X))) + (Abs(int(pack.Y) - int(listpal[i].Y)))
 		if distance > tmp {
 			ret = i
 			distance = tmp
@@ -195,6 +198,7 @@ func get_pal(distance int, pack packet, Listpal []palette) int {
 	return ret
 }
 
+// Find find if a value exist in a slice of int
 func Find(slice []int, index int) bool {
 	for _, i := range slice {
 		if i == index {
@@ -204,6 +208,7 @@ func Find(slice []int, index int) bool {
 	return false
 }
 
+// executeAlgorithm Execute all the algorithm and write the output of the program
 func executeAlgorithm(warehouse warehouseMap) bool {
 	algo := algorithm{Ware: warehouse, TwoDMap: make([][]uint8, warehouse.Y, warehouse.X)}
 	for i := 0; i < int(algo.Ware.NbIter); i++ {
@@ -213,7 +218,7 @@ func executeAlgorithm(warehouse warehouseMap) bool {
 		fmt.Printf("\n")
 		for packindex := 0; packindex < len(algo.Listpacket); packindex++ {
 			distance := algo.Ware.X * algo.Ware.Y
-			tmp := get_pal(int(distance), algo.Listpacket[packindex], algo.Listpal)
+			tmp := getPal(int(distance), algo.Listpacket[packindex], algo.Listpal)
 			if tmp != -1 {
 				move = append(move, tmp)
 				algo.gotopacket(tmp, packindex)
@@ -224,7 +229,7 @@ func executeAlgorithm(warehouse warehouseMap) bool {
 				continue
 			}
 			distance := algo.Ware.X * algo.Ware.Y
-			tmp := get_truck(int(distance), algo.Listpal[palindex], algo.Listtruck)
+			tmp := getTruck(int(distance), algo.Listpal[palindex], algo.Listtruck)
 			if tmp != -1 {
 				move = append(move, tmp)
 				algo.gototruck(palindex, tmp)
@@ -240,7 +245,7 @@ func executeAlgorithm(warehouse warehouseMap) bool {
 		}
 		for _, truck := range algo.Listtruck {
 			if truck.Round > 0 {
-				truck.Round -= 1
+				truck.Round--
 				fmt.Printf("%s GONE %d/%d", truck.Name, truck.Content, truck.MaxContent)
 				if truck.Round == 0 {
 					truck.Content = 0
